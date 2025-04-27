2da769c331cbb73de5e680d19ae0bd7ee4a5c3ff
Atepazi86 <140503795+Atepazi86@users.noreply.github.com>
2025-04-27 20:33:43 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Atep Aji Ahmad fauzi</title>
+</head>
+<body>
+  <table border="1" cellspacing="0" cellpadding="10" width="700">
+    <tr>
+      <th colspan="3" bgcolor="#FFCC99" align="center">
+        <font size="5">DAFTAR PRODUK</font>
+      </th>
+    </tr>
+    <tr>
+      <td rowspan="3" bgcolor="#FFCCCC" align="center">
+        <img src="https://th.bing.com/th/id/OIP._kl0KhDG8q8tmSnWDtAStQHaHa?rs=1&pid=ImgDetMain" alt="Axioo Pongo 725" width="120"><br>
+        axio pongo 725
+      </td>
+      <td bgcolor="#CCFFCC">Nama<br>produk</td>
+      <td bgcolor="#CCCCFF">AXIOO PONGO 725</td>
+    </tr>
+    <tr>
+      <td bgcolor="#CCFFCC">Harga</td>
+      <td bgcolor="#CCCCFF">Rp.10.000.000</td>
+    </tr>
+    <tr>
+      <td bgcolor="#CCFFCC">Fitur <br>Produk</td>
+      <td bgcolor="#CCCCFF">
+        Prosesor: Intel® Core™ i7-12650H <br>
+        Grafis: Intel® Graphics <br> 
+        NVIDIA® GeForce RTX™ 2050 <br>
+        Layar: 15.6 inch Display <br>
+        Penyimpanan: 512GB SSD <br>
+        RAM: 16GB DDR4
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
